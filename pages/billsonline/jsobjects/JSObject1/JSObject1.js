7ec1deb785d0b17e61c34b0db9938b9490483f75
saikumar <ece.saikumar@gmail.com>
2023-08-01 23:28:20 +0000

    3

--- pages/billsonline/jsobjects/JSObject1/JSObject1.js
+++ pages/billsonline/jsobjects/JSObject1/JSObject1.js
@@ -2,7 +2,7 @@ export default {
 	myVar1: [],
 	myVar2: {},
 	myFun1 () {
-
+    storeValue('appsmith_host','https://0a25-144-24-135-94.ngrok-free.app/');
 		storeValue('rent_paid_status',appsmith.URL.queryParams['rent_paid_status']);
 		storeValue('tenant_id',appsmith.URL.queryParams['tenant_id'])
 		storeValue('tabs_l','login1');
--- pages/main/jsobjects/JSObject1/JSObject1.js
+++ pages/main/jsobjects/JSObject1/JSObject1.js
@@ -2,7 +2,8 @@ export default {
   myVar1: [],
   myVar2: {},
   myFun1 () {
-		storeValue('link_url','https://app.appsmith.com/app/billsonline/billsonline-64c6cee62d8f7a159ce65dc3?tenant_id=1&rent_paid_status=1');
+		storeValue('appsmith_host','https://0a25-144-24-135-94.ngrok-free.app');
+		storeValue('link_url',appsmith.store.appsmith_host + '/app/https://0a25-144-24-135-94.ngrok-free.app/app/billsrents/billsonline-64c9835099650e433ab41443?tenant_id=1&rent_paid_status=1&embed=');
 		storeValue('st', 'https://api.callmebot.com/whatsapp.php?phone=+918885305097&text= ' + ShortenURL.data + '&apikey=3103785');
     storeValue('tabs_m', '0');
     storeValue('tabs_e', '1');
